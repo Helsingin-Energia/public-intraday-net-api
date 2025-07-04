dbffcb83e3804949936579825d6170568b2d7510
Lassi Helynranta <lassi.helynranta@helen.fi>
2025-07-04 11:13:28 +0300

    Minor styling changes and suppression file

---
+++ NPS.ID.PublicApi.Models/GlobalSuprressions.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
+// I´m not fixing styling in forked Nordpool API project, as it is not my code and I do not want to break the original code style.
+[assembly: SuppressMessage("Style", "MA0016:File name must match type name")]
+[assembly: SuppressMessage("Style", "MA0049: Type name should not match containing namespace")]
+[assembly: SuppressMessage("Roslynator", "RCS1037:Remove trailing white-space")]
+[assembly: SuppressMessage("Roslynator", "RCS1161: Enum should declare explicit values ")]
+[assembly: SuppressMessage("Style", "CA1707: Remove the underscores from member name")]
+[assembly: SuppressMessage("Style", "CA1711: Rename type name OrderOwnershipEnum so that it does not end in 'Enum'")]
+[assembly: SuppressMessage("Style", "CA1716: Rename namespace")]
